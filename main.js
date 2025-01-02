91a9bf14159721fe315438851d2bbf29345669dc
AidanRansonCollege <aidanransoncollege@gmail.com>
2025-01-02 14:32:13 -0500

    LinearB Added

--- main.js
+++ main.js
@@ -10,13 +10,16 @@ var endTimeTesting;
 var timeElapsedTesting;
 /////////////////
 var destination = "testing.html"
-var availableChar = ['A', 'B', 'C','D', 'E', 'F'];
+const LinearB = "𐀀𐀁𐀂𐀃𐀄𐀅𐀆𐀇𐀈𐀉𐀊𐀋𐀍𐀎𐀏𐀐𐀑𐀒𐀓𐀔𐀕𐀖𐀗𐀘𐀙𐀚𐀛𐀜𐀝𐀞𐀟𐀠𐀡𐀢𐀣𐀤𐀥𐀦𐀨𐀩𐀪𐀫𐀬𐀭𐀮𐀯𐀰𐀱𐀲𐀳𐀴𐀵𐀶𐀷𐀸𐀹𐀺𐀼𐀽𐀿𐁀𐁁𐁂𐁃𐁄𐁅𐁆𐁇𐁈𐁉𐁊𐁋𐁌𐁍";
+var availableChar = Array.from(LinearB);
+var testingChar;
 
 //Global Functions
 function OutputProgramming(){
     console.log(timeElapsedProgramming/1000 + "s");
     console.log(isCorrectProgramming);
-    window.location.href = destination;
+    //COMMENT WHEN SHOWING OUTPUT FIRST TIME
+    //window.location.href = destination;
 }
 
 function IncorrectProgramming(){
@@ -49,9 +52,16 @@ function ProgrammingPhase(){
     var goalChar = Math.floor(Math.random() * availableChar.length);
     var trueButton = buttons[trueButtonIndex];
 
-    trueButton.textContent = availableChar[goalChar];
-    goal.textContent = availableChar[goalChar];
+    testingChar = availableChar[goalChar];
+    availableChar.splice(goalChar,1);
+    trueButton.textContent = testingChar;
+    goal.textContent = testingChar;
+    
 
     trueButton.removeEventListener("click", IncorrectProgramming);
     trueButton.addEventListener("click", CorrectProgramming);
+}
+
+function TestingPhase(){
+
 }
